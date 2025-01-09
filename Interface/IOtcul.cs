790635df6db9a634e107f2e2f48e18420ae624fa
ilkay-onay <ilkayonay2001@gmail.com>
2025-01-10 01:25:14 +0300

    artık dosyaları sil

--- Interface/IOtcul.cs
+++
@@ -1,6 +0,0 @@
-// otçul hayvanların interface'ini tanımlar.
-// otçul hayvanlar, avlanma özelliğine sahip değildir.
-namespace HayvanatBahcesiSimulasyonu.Models
-{
-    public interface IOtcul : IHayvan { }
-}
